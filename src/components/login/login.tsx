5a54d8d8a6f18c39a83451e0c8ada869137de4d0
Israel Kusayev <israel.kusayev@gmail.com>
2019-10-27 22:15:52 +0200

    :alembic: trying scss modules

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import styles from './login.module.scss';
 
 interface LoginProps {}
 interface LoginState {}
 
 export default class Login extends Component<LoginProps, LoginState> {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <input className={styles.input + ' has-error'} type='text' />
+      </div>
+    );
   }
 }
